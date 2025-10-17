22e14e1a3d1e5a25514456b4986e377bee5a7a00
tienlong <letienlongmc2003@gmail.com>
2025-10-17 22:48:10 +0700

    fix file 2

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -4,32 +4,48 @@ import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Public, ResponseMessage } from 'src/decorator/customize';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService) { }
 
   @Post('upload')
   @ResponseMessage("Upload a file")
-  @UseInterceptors(FileInterceptor('fileUpload'))
+  @UseInterceptors(FileInterceptor('fileUpload',
+    {
+      storage: diskStorage({
+        // ✅ Lưu file vào thư mục "resume" thay vì "default"
+        destination: './public/images/resume',
+        filename: (req, file, cb) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const baseName = file.originalname.replace(ext, '');
+          cb(null, `${baseName}-${uniqueSuffix}${ext}`);
+        },
+      }),
+    }
+  ),
+  )
   uploadFile(@UploadedFile(
     new ParseFilePipeBuilder()
-    .addFileTypeValidator({
-      fileType: /^(jpg|image\/jpeg|jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/i,
-    })
-    .addMaxSizeValidator({
-      maxSize: 1024*1024 //kb
-    })
-    .build({
-      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY // ma loi 422
-    }),
+      .addFileTypeValidator({
+        fileType: /^(jpg|image\/jpeg|jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/i,
+      })
+      .addMaxSizeValidator({
+        maxSize: 1024 * 1024 //kb
+      })
+      .build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY // ma loi 422
+      }),
 
   ) file: Express.Multer.File) {
-  return {
-    fileName:file.filename,
-    path: `/public/images/${file.filename}`, // Trả về đường dẫn file
+    return {
+      fileName: file.filename,
+      path: `/public/images/${file.filename}`, // Trả về đường dẫn file
+    }
   }
-}
 
   @Get()
   findAll() {
