7037312388ca519dbef7b564cb30926ba12f66b6
Lê Tiến Long <122037484+tienlongmc@users.noreply.github.com>
2026-01-23 11:08:56 +0700

    Comment out JWT guard middleware
    Commented out the JWT guard middleware for testing.

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,7 @@ async function bootstrap() {
   optionsSuccessStatus: 204,
 });
   
+  
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
     app.use((req, res, next) => {
@@ -46,7 +47,7 @@ async function bootstrap() {
   });
 
   // JWT Guard
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  // app.useGlobalGuards(new JwtAuthGuard(reflector));
 
   // Static files & views
   app.useStaticAssets(join(__dirname, '..', 'public')); // truy cập js, css, img
